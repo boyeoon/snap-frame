7d9f000c9ce387f7ef80e6e3bff33824f5a0611d
Boyeon Jang <uko010199@gmail.com>
2024-10-11 04:40:27 +0900

    refactor: remove setVideoSrc function
    - deleted unused setVideoSrc function for cleaner code

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -9,26 +9,24 @@ interface CameraProps {
 export default function Camera({ setVideoSrc }: CameraProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  // const startCamera = async () => {
-  //   try {
-  //     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  //     setVideoSrc(stream);
-  //     if (videoRef.current) {
-  //       videoRef.current.srcObject = stream;
-  //       videoRef.current.play();
-  //     }
-  //   } catch (error) {
-  //     console.error("카메라를 켜는 데 실패했습니다:", error);
-  //     alert("카메라를 켜는 데 실패했습니다.");
-  //   }
-  // };
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      setVideoSrc(stream);
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      }
+    } catch (error) {
+      console.error("카메라를 켜는 데 실패했습니다:", error);
+      alert("카메라를 켜는 데 실패했습니다.");
+    }
+  };
 
   return (
     <div className="flex flex-col items-center mt-4">
-      {/* <button onClick={startCamera} className="bg-blue-500 text-white py-2 px-4 rounded">
-        카메라 켜기
-      </button> */}
       <video ref={videoRef} className="hidden" />
     </div>
   );
 }
+
