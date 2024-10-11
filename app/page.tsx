258474db83ca102d56f9cf908c86cb9721cc5d48
Boyeon Jang <uko010199@gmail.com>
2024-10-11 19:38:48 +0900

    fix: update font size

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-3xl font-bold my-4">SNAP FRAME</h1>
+      <h1 className="text-7xl font-bold my-4">SNAP FRAME</h1>
       <LifePhoto />
     </div>
   );
