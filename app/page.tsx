fa3c498ab83ddbaa68ad2e5150159d7aef264690
Boyeon Jang <uko010199@gmail.com>
2024-10-14 19:18:55 +0900

    style: update title styles
    - add shadow
    - color changes: #cb6872
    - letter spacing: .2rem

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-7xl font-bold my-4">SNAP FRAME</h1>
+      <h1 className="text-7xl font-normal tracking-[.2rem] drop-shadow-md mt-20 mb-4 text-[#cb6872]">SNAPFRAME</h1>
       <LifePhoto />
     </div>
   );
