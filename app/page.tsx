283293febe6577cd78b486cf94d017f571641f7d
Boyeon Jang <uko010199@gmail.com>
2024-10-15 23:57:14 +0900

    feat: update favicon link code

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,9 @@ import LifePhoto from "./components/LifePhoto";
 export default function Home() {
   
   return (
-    <div>
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
-      <main className="flex flex-col items-center">
-        <h1 className="text-7xl font-normal tracking-[.2rem] drop-shadow-md mt-20 mb-4 text-[#cb6872]">SNAPFRAME</h1>
-        <LifePhoto />
-      </main>
+    <div className="flex flex-col items-center">
+      <h1 className="text-7xl font-normal tracking-[.2rem] drop-shadow-md mt-20 mb-4 text-[#cb6872]">SNAPFRAME</h1>
+      <LifePhoto />
     </div>
   );
 }
