8117fb0cc48d26aa273bd4714bf01120f84be019
Boyeon Jang <uko010199@gmail.com>
2024-11-02 22:58:56 +0900

    refactor: remove comments

--- app/components/Camera.tsx
+++ app/components/Camera.tsx
@@ -3,26 +3,11 @@
 import React, { useRef } from "react";
 
 interface CameraProps {
-  setVideoSrc: (src: MediaStream | null) => void; 
+  setVideoSrc: (src: MediaStream | null) => void;
 }
 
-// export default function Camera({ setVideoSrc }: CameraProps) {
-  export default function Camera({ }: CameraProps) {
+export default function Camera({}: CameraProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
-
-  // const startCamera = async () => {
-  //   try {
-  //     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  //     setVideoSrc(stream);
-  //     if (videoRef.current) {
-  //       videoRef.current.srcObject = stream;
-  //       videoRef.current.play();
-  //     }
-  //   } catch (error) {
-  //     console.error("카메라를 켜는 데 실패했습니다:", error);
-  //     alert("카메라를 켜는 데 실패했습니다.");
-  //   }
-  // };
 
   return (
     <div className="flex flex-col items-center mt-4">
@@ -30,4 +15,3 @@ interface CameraProps {
     </div>
   );
 }
-
--- app/components/FrameSelect.tsx
+++ app/components/FrameSelect.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import OneByFourLayout from "../framelayout/OneByFourLayout";
 import TwoByTwoLayout from "../framelayout/TwoByTwoLayout";
-import ThreeByOneLayout from "../framelayout/ThreeByOneLayout"
+import ThreeByOneLayout from "../framelayout/ThreeByOneLayout";
 
 interface FrameListProps {
-  videoSrc: MediaStream | null; 
+  videoSrc: MediaStream | null;
   startCamera: () => Promise<void>; // 카메라 시작 함수 추가
 }
 
 const frames = [
   { id: 1, label: "one by four" },
   { id: 2, label: "two by two" },
-  { id: 3, label: "three by one" }
+  { id: 3, label: "three by one" },
 ];
 
 export default function FrameList({ videoSrc, startCamera }: FrameListProps) {
@@ -21,37 +21,40 @@ export default function FrameList({ videoSrc, startCamera }: FrameListProps) {
 
   return (
     <div className="flex flex-col items-center">
-      <select 
+      <select
         className="mb-8 p-2 outline-none border rounded-lg text-white bg-[#ca3c4a]/60"
         onChange={(e) => setSelectedFrame(Number(e.target.value))}
         defaultValue=""
       >
-        <option value="" disabled>Select Frame</option>
+        <option value="" disabled>
+          Select Frame
+        </option>
         {frames.map(({ id, label }) => (
-          <option key={id} value={id}>{label}</option>
+          <option key={id} value={id}>
+            {label}
+          </option>
         ))}
       </select>
 
       {selectedFrame === 1 && (
-        <OneByFourLayout 
-          videoSrc={videoSrc} 
-          // label={`${frames.find(frame => frame.id === selectedFrame)?.label}`} 
+        <OneByFourLayout
+          videoSrc={videoSrc}
           label={` `}
           startCamera={startCamera} // 카메라 시작 함수 전달
         />
       )}
-      
+
       {selectedFrame === 2 && (
-        <TwoByTwoLayout 
-          videoSrc={videoSrc} 
+        <TwoByTwoLayout
+          videoSrc={videoSrc}
           label={` `}
           startCamera={startCamera}
         />
       )}
 
       {selectedFrame === 3 && (
-        <ThreeByOneLayout 
-          videoSrc={videoSrc} 
+        <ThreeByOneLayout
+          videoSrc={videoSrc}
           label={` `}
           startCamera={startCamera}
         />
